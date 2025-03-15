e660dd984e0aa57f52baed0e2d259579105d57cb
Tasmagambetov Andrey <theiznogoud@gmail.com>
2025-03-15 22:23:57 +0300

    made filter function

--- filtering/include/filtering/filtering.cuh
+++ filtering/include/filtering/filtering.cuh
@@ -7,19 +7,23 @@
 __global__ void memoryRecalculationKernel(float* memory, float* memoryTemp, const float *numerator, const float *denumerator,
                                           const float inputSignalSample, const float outputSignalSample, const unsigned int order){
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
-    if (tid < order - 1){
+    if (tid <= order - 2){
         memory[tid] = memoryTemp[tid + 1] + numerator[tid + 1] * inputSignalSample - denumerator[tid + 1] * outputSignalSample;
-        printf("Thread %d: delayElements[%d] = %f\n", tid, tid, memory[tid]);
-    } 
+    } else {
+        if (tid == order - 1){
+            memory[tid] = numerator[tid + 1] * inputSignalSample - denumerator[tid + 1] * outputSignalSample;
+        }
+    }
 }
 
 /// функция фильтрации сигнала, выполняется на CPU
 void filter(std::vector<float>& inputSignal, std::vector<float>& outputSignal,
             std::vector<float>& numerator, std::vector<float>& denumerator){
     unsigned int order = numerator.size() > denumerator.size() ? numerator.size() : denumerator.size();
+    order--;
 
-    std::vector<float> memory(order - 1, 0.0);
-    std::vector<float> memoryTemp(order - 1, 0.0);
+    std::vector<float> memory(order, 0.0);
+    std::vector<float> memoryTemp(order, 0.0);
 
     float *numeratorPtr, *denumeratorPtr, *memoryPtr, *memoryTempPtr;
     cudaMalloc(&numeratorPtr, numerator.size() * sizeof(float));
@@ -35,7 +39,6 @@ void filter(std::vector<float>& inputSignal, std::vector<float>& outputSignal,
 
     for (unsigned int k = 0; k < inputSignal.size(); ++k){
         outputSignal[k] = (inputSignal[k] * numerator[0] + memory[0]) * denumerator[0];
-//        std::cout << outputSignal[k] << std::endl;
 
         cudaMemcpy(memoryPtr, memory.data(), memory.size() * sizeof(float), cudaMemcpyHostToDevice);
         cudaMemcpy(memoryTempPtr, memoryTemp.data(), memoryTemp.size() * sizeof(float), cudaMemcpyHostToDevice);
@@ -43,11 +46,8 @@ void filter(std::vector<float>& inputSignal, std::vector<float>& outputSignal,
         memoryRecalculationKernel<<<gridSize, blockSize>>>(memoryPtr, memoryTempPtr, numeratorPtr, denumeratorPtr,
                                                            inputSignal[k], outputSignal[k], order);
 
-        cudaMemcpy(memory.data(), memoryPtr, order * sizeof(float), cudaMemcpyDeviceToHost);
-        cudaMemcpy(memoryTemp.data(), memoryPtr, order * sizeof(float), cudaMemcpyDeviceToHost);
-//        std::cout << inputSignal[k] << " " << outputSignal[k] << " " << memory[0] << " " << memory[1] << std::endl;
-//        std::cout << memoryTemp[0] << " " << memoryTemp[1] << std::endl;
-//        std::cout << std::endl;
+        cudaMemcpy(memory.data(), memoryPtr, memory.size() * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaMemcpy(memoryTemp.data(), memoryPtr, memoryTemp.size() * sizeof(float), cudaMemcpyDeviceToHost);
     }
 
     cudaFree(numeratorPtr);
--- main.cu
+++ main.cu
@@ -8,11 +8,10 @@
 
 int main() {
 
-    std::vector<float> inputSignal = {1, 0, 0 ,0 ,0};
+    std::vector<float> inputSignal = {1, 0, 0 ,0 ,0, 0, 0, 0, 0, 0};
     std::vector<float> outputSignal(inputSignal.size());
-    std::vector<float> numerator = {0.9, 0.1, -0.2}; // нерекурсивная часть
-    std::vector<float> denumerator = {1, 0.5, 0.01}; // рекурсивная часть
-    std::vector<float> memory(inputSignal.size(), 0.0);
+    std::vector<float> numerator = {0.9, 0.1, -0.2}; // нерекурсивная часть, кэфы bi
+    std::vector<float> denumerator = {1, 0.5, 0.01}; // рекурсивная часть, кэфы ai
 
     filter(inputSignal, outputSignal, numerator, denumerator);
 
